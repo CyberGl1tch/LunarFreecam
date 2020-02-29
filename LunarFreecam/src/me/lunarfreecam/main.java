28b4038d024d105b8cea96687540c3b1b125b42f
magas8 <komavrellis@gmail.com>
2020-02-29 19:51:14 +0200

    Adding Inteliji Project

---
+++ LunarFreecam/src/me/lunarfreecam/main.java
@@ -0,0 +1,14 @@
+package me.lunarfreecam;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin {
+    @Override
+    public void onEnable(){
+
+    }
+
+    public void onDisable(){
+
+    }
+}
