9c530567835ab065f752430e98c354713137ab39
Cyb3r6host <43292879+Cyb3r6host@users.noreply.github.com>
2020-11-15 13:25:34 +0200

    Update For 1.16.4

--- src/main/java/lunarfreecam/freecam/Handler.java
+++ src/main/java/lunarfreecam/freecam/Handler.java
@@ -80,7 +80,7 @@ public class Handler implements Listener {
 			player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("freecam-spectate-teleport")));
 			e.setCancelled(true);
 		}
-		if (Main.playersInFreecam.stream().anyMatch(c -> c.getLocation().getChunk().equals(e.getTo().getChunk()))){
+		if (!Main.playersInFreecam.contains(e.getPlayer()) && Main.playersInFreecam.stream().anyMatch(c -> c.getLocation().getChunk().equals(e.getTo().getChunk()))){
 			e.getPlayer().sendMessage(utils.Color(plugin.getConfig().getString("freecam-tp-while-in-freecam")));
 			e.setCancelled(true);
 		}
